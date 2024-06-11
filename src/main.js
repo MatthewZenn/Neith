79df02ed9ccd5ebe5f3a0e3975e6ea0d96944b1a
Matthew Zenn <spyguy57@gmail.com>
2024-06-11 19:15:08 -0400

    updated ui

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
 document.getElementById('titlebar-close').addEventListener('click', () => window.close());
 var editor = ace.edit("editor");
 
-editor.session.setMode("ace/mode/javascript");
+editor.session.setMode("ace/mode/css");
